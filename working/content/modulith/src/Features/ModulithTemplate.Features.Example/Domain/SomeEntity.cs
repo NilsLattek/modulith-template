55f0590c545e264c199ff3f6c3a36b855462ad76
Nils Lattek <470938+NilsLattek@users.noreply.github.com>
2026-07-20 20:21:00 +0200

    refactor: Project structure (#4)

--- working/content/modulith/src/Features/ModulithTemplate.Features.Example/Domain/SomeEntity.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ModulithTemplate.Features.Example;
-
-#pragma warning disable S2094 // Classes should not be empty
-public class SomeEntity
-#pragma warning restore S2094 // Classes should not be empty
-{
-
-}
---
+++ working/content/modulith/src/Features/Orders/ModulithTemplate.Features.Orders.Domain/Entities/SomeEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace ModulithTemplate.Features.Example;
+
+#pragma warning disable S2094 // Classes should not be empty
+public class SomeEntity
+#pragma warning restore S2094 // Classes should not be empty
+{
+
+}
